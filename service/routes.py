c3bbe0941d4b43203339030d28141a6d790eb4b0
AlexMGH <alexandru.mihai.aa@gmail.com>
2023-03-15 14:51:04 +0200

    Update routes.py
    Signed-off-by: AlexMGH <alexandru.mihai.aa@gmail.com>

--- service/routes.py
+++ service/routes.py
@@ -71,7 +71,7 @@ def list_accounts():
     app.logger.info("Request to list accounts")
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-
+    
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -91,8 +91,8 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-
-
+    
+    
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -110,8 +110,8 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
-
-
+    
+    
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
